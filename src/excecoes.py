9a315be9cf3ba715de1767c63ddf896023294a56
Guilherme Sena <guiisena2022@gmail.com>
2025-02-25 20:17:52 -0300

    Update excecoes.py

--- src/excecoes.py
+++ src/excecoes.py
@@ -1,27 +1,20 @@
 class UJCException(Exception):
-    """Exceção personalizada para erros relacionados a UJC."""
     pass
 
 class UNCException(Exception):
-    """Exceção personalizada para erros relacionados a UNC."""
     pass
 
 class PIException(Exception):
-    """Exceção personalizada para erros relacionados a PI."""
     pass
 
 class PDException(Exception):
-    """Exceção personalizada para erros relacionados a PD."""
     pass
 
 class MFPException(Exception):
-    """Exceção personalizada para erros relacionados a MFP."""
     pass
 
 class PEException(Exception):
-    """Exceção personalizada para erros relacionados a PE."""
     pass
 
 class SIException(Exception):
-    """Exceção personalizada para erros relacionados a SI."""
     pass
